2874495f13c74739ef342291ab46304d6ac1365b
SenulRajapakshe <SenulRajapakshe34@gmail.com>
2025-03-09 22:06:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&saVAXZzS#c81T2WK5gQSUr-d3Bi3x_93SCQQU_SCrkjIe_wAJwVM' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
